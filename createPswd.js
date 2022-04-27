bbb88e76eecd37a5d904404448ebdf020ada1d0c
Khailas R <khailas303@gmail.com>
2022-04-27 18:36:44 +0530

    pswd create

---
+++ createPswd.js
@@ -0,0 +1,16 @@
+const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*_-+=';
+
+
+const createPswd = (
+    length = '8', hasNumbers = true, hasSymbols = true
+) => {
+    let chars = letters;
+    hasNumbers ? (chars += numbers) : '';
+    hasSymbols ? (chars += symbols) : '';
+
+    return chars;
+}
+
+module.exports = createPswd
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const program = require('commander');
+const log = console.log();
+
+const createPswd = require('createPswd');
 
 
 program.version('1.0.0').description('Password Generator');
@@ -6,8 +9,16 @@ program.version('1.0.0').description('Password Generator');
 
 const prog = program.
     option('-l, --length <number>', 'Length of Password', '8')
-    .option('-s, --save', 'Save Passwords to pswd.txt', '8')
+    .option('-s, --save', 'Save Passwords to pswd.txt')
+    .option('-rm, --no-numbers', 'Remove Numbers')
+    .option('-rs, --no-symbols', 'Remove Symbols')
 
     .parse();
 
-console.log(prog.opts());
+
+const { length, save, numbers, symbols } = prog.opts();
+
+
+const generatedPswd = createPswd(length, numbers, symbols) ;
+
+log(generatedPswd);
